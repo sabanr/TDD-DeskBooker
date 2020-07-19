c892a8f47b7905d793a5294c0c3c9770be7e1352
sabanr <sabanr@users.noreply.github.com>
2020-07-19 12:55:24 -0300

    Create first test

---
+++ DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessorTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+
+namespace DeskBooker.Core.Processor {
+	public class DeskBookingRequestProcessorTests {
+
+		[Fact]
+		public void Should_return_deskbooking_result_with_request_values() {
+
+			// arrange
+			var request = new DeskBookingRequest {
+				FirstName = "Roberto",
+				LastName = "Saban",
+				Email = "roberto.saban@me.to",
+				Date = new DateTime(2020, 01, 28)
+			};
+
+			// act
+			var processor = new DeskBookingRequestProcessor();
+			DeskBookingResult result = processor.BookDesk(request);
+
+			// asset
+			Assert.NotNull(result);
+			Assert.Equal(request.FirstName, result.FirstName);
+			Assert.Equal(request.LastName, result.LastName);
+			Assert.Equal(request.Email, result.Email);
+			Assert.Equal(request.Date, result.Date);
+
+		}
+
+	}
+}
---
+++ DeskBooker.Core/DeskBookingRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DeskBooker.Core.Processor {
+	public class DeskBookingRequest {
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Email { get; set; }
+		public DateTime Date { get; set; }
+	}
+}
